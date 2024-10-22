b910e7fe820714e3e5169761dc304291ff040a0f
mori-318 <minotuyu31851423520@gmail.com>
2024-10-22 22:37:14 +0900

    目元モザイク機能の実装完了

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI, File, UploadFile, Query
 from pydantic import BaseModel
-import numpy as np
 from typing import List
-import cv2
-import requests
-from fastapi.encoders import jsonable_encoder
 
 from libs.classify_img_atmos.classify_atmos import ClassifyAtmos
 from libs.apply_filters.atmos_changer import AtmosChanger
@@ -22,8 +18,8 @@ async def classify_atmos(img_file: UploadFile = File(...)):
         r"assets\model_weight\smile_model_weight"
     )
     result, is_face = classify_atmos.run_classify(img)
-
     return {"atmos": result, "isFace": is_face}
+
 
 @app.post("/apply_filters")
 async def apply_filters(applied_filters: List[str] = Query(...), img_file: UploadFile = File(...)):
@@ -31,8 +27,8 @@ async def apply_filters(applied_filters: List[str] = Query(...), img_file: Uploa
     atmos_changer = AtmosChanger(img, applied_filters)
     processed_img = atmos_changer.run_atmos_change()
     processed_img_file = utils.img_to_file(processed_img)
-
     return {"status": "processed", "imgFile": processed_img_file}
+
 
 @app.get("/")
 def read_root():
