914cdaf6ffd9e3a70fe4c607e1cf85ac80a99a18
mori-318 <minotuyu31851423520@gmail.com>
2024-10-26 15:52:16 +0900

    チャット画面で使うアイコンを追加

--- frontend/src/chat/chat.js
+++ frontend/src/chat/chat.js
@@ -1,28 +1,9 @@
 // クリックイベントを適切に設定
 var newtalk=document.getElementById('newtalk');
 
-if(newtalk){
-    
-    event.preventDefault();
-}
-
 
 var textUploadBtn = document.getElementById('textUploadBtn');
 
-// クリックイベントがバインドされているかどうか確認
-// if (textUploadBtn) {
-//     textUploadBtn.addEventListener('click', function() {
-//         console.log('ok');
-//         const chatDisplay = document.getElementById('chatDisplay');
-//         const div = document.createElement('div');
-//         const p = document.createElement('p');
-//         p.textContent = "新しい文字っすンご";
-//         div.appendChild(p);
-//         chatDisplay.appendChild(div);
-//     }, false);
-// } else {
-//     console.error('textUploadBtn が見つかりません');
-// }
 if (textUploadBtn) {
     textUploadBtn.addEventListener('click', function() {
         console.log('ok');
