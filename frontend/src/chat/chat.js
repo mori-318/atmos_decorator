c6361c5952fed97950c6cc9eba11f57771ec3880
Rmatsuoka0518 <ryosuke518371@gmail.com>
2024-10-26 16:06:28 +0900

    ンゴォォォgit add .git add .!

--- frontend/src/chat/chat.js
+++ frontend/src/chat/chat.js
@@ -1,28 +1,7 @@
 // クリックイベントを適切に設定
-var newtalk=document.getElementById('newtalk');
-
-if(newtalk){
-    
-    event.preventDefault();
-}
-
-
 var textUploadBtn = document.getElementById('textUploadBtn');
 
 // クリックイベントがバインドされているかどうか確認
-// if (textUploadBtn) {
-//     textUploadBtn.addEventListener('click', function() {
-//         console.log('ok');
-//         const chatDisplay = document.getElementById('chatDisplay');
-//         const div = document.createElement('div');
-//         const p = document.createElement('p');
-//         p.textContent = "新しい文字っすンご";
-//         div.appendChild(p);
-//         chatDisplay.appendChild(div);
-//     }, false);
-// } else {
-//     console.error('textUploadBtn が見つかりません');
-// }
 if (textUploadBtn) {
     textUploadBtn.addEventListener('click', function() {
         console.log('ok');
@@ -31,13 +10,13 @@ if (textUploadBtn) {
         const p = document.createElement('p');
         p.textContent = "新しい文字っすンご";
         div.appendChild(p);
+        div.classList.add("userChat", "col-12", "col-md-6", "col-sm-12", "ml-auto");
         chatDisplay.appendChild(div);
-        // p.textContent='';
-        // textUploadBtn.value='';
     }, false);
 } else {
     console.error('textUploadBtn が見つかりません');
 }
+
 
 
 
