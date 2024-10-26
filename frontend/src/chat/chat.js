ab971a4b6065a3ebf9c536fbf4e86347dd5935da
mori-318 <minotuyu31851423520@gmail.com>
2024-10-26 22:31:09 +0900

    とりあえず

--- frontend/src/chat/chat.js
+++ frontend/src/chat/chat.js
@@ -1,16 +1,14 @@
-var textUploadBtn = document.getElementById('textUploadBtn');
+var textUploadBtn = document.getElementById('textUploadButton');
 
 if (textUploadBtn) {
     textUploadBtn.addEventListener('click', function() {
         console.log('ok');
-        const chatDisplay = document.getElementById('chatDisplay');
+        const chatDisplay = document.getElementById('border-chat');
         const div = document.createElement('div');
         const p = document.createElement('p');
         p.textContent = "新しい文字っすンご";
         div.appendChild(p);
         chatDisplay.appendChild(div);
-        // p.textContent='';
-        // textUploadBtn.value='';
     }, false);
 } else {
     console.error('textUploadBtn が見つかりません');
